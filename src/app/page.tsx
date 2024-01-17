d10bcc6938b7bfc55bddf78a7f0a3af7cabd05fa
thomas <thomashamon@hotmail.fr>
2024-01-17 10:00:36 +0100

    add correct shadcn component for input & title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-16 p-24">
-      <h1>Project Shotgun</h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Project Shotgun
+      </h1>
       <div className="gap-4 flex flex-row">
-        <input type="text" placeholder="Pseudo" />
+        <Input type="text" placeholder="Pseudo" />
         <Button
           title="Créer une bagarre"
           onClick={() => {
